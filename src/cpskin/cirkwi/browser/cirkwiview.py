55bd491c911d1126795bce71910dfd38599c97df
boulch <christophe.boulanger@imio.be>
2015-10-06 10:13:36 +0200

    First commit

---
+++ src/cpskin/cirkwi/browser/cirkwiview.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import urllib2
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class CirkwiView(BrowserView):
+    index = ViewPageTemplateFile("cirkwiview.pt")
+    cdf_parametres_client = ''
+    em_name = ''
+
+    def render(self):
+        return self.index()
+
+    def cdf_parametres(self):
+        # String sample for "comblain-au-pont" : "cdf_id_host:3249,cdf_id_outil:570,cdf_lang:fr"
+        cdf_host = "cdf_id_host:" + self.context.cdf_host
+        cdf_outils = "cdf_id_outils:" + self.context.cdf_outils
+        if self.context.cdf_lang is None:
+            # no language was set in cirkwi object so we use Plone instance language.
+            self.context.cdf_lang = self.context.language
+        cdf_lang = "cdf_lang:" + self.context.cdf_lang
+        return "%s,%s,%s" % (cdf_host, cdf_outils, cdf_lang)
+
+    def __call__(self):
+        return self.render()
+
+    def getpage(self, cdf_parametres_client='', em_name=''):
+        cdf_page_url = 'http://outil.cirkwi.com/outil/client_recherche_page_client.php'
+        cdf_file_url = 'http://outil.cirkwi.com/outil/client_recherche_fichier_client.php'
+        try:
+            qs = self.request.form
+            if 'type' not in qs:  # we must get a page
+                cdf_url = "%s?%s" % (cdf_page_url, qs)
+                if 'cdf_parametres_client' not in qs:
+                    cdf_url = "%s&cdf_parametres_client=%s" % (cdf_url, cdf_parametres_client)
+            else:  # we must get a file
+                cdf_url = "%s?%s" % (cdf_file_url, qs)
+                if qs['type'] == 'xml':
+                    self.request.response.setHeader('content-type', 'text/xml')
+                elif qs['type'] == 'texte':
+                    self.request.response.setHeader('content-type', 'text/html')
+                elif qs['type'] == 'image':
+                    self.request.response.setHeader('content-type', 'text/jpeg')
+            conn = urllib2.urlopen(cdf_url)
+            data = conn.read()
+            conn.close()
+            data = data.replace('href="page_membre.php?',
+                                'target="_blank" href="http://www.circuits-de-belgique.be/page_membre.php?')
+            data = data.replace('href="page.php?',
+                                'target="_blank" href="http://www.circuits-de-belgique.be/page.php?')
+            if data == '2':
+                raise Exception('Look at your host id!')
+            return data
+        except Exception, msg:
+            return("Cannot open url '%s': %s" % (cdf_url, msg))
---
+++ src/cpskin/cirkwi/testing.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
+from plone.app.testing import applyProfile
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from plone.testing import z2
+
+import cpskin.cirkwi
+
+
+class CpskinCirkwiLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        self.loadZCML(package=cpskin.cirkwi)
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'cpskin.cirkwi:default')
+
+
+CPSKIN_CIRKWI_FIXTURE = CpskinCirkwiLayer()
+
+
+CPSKIN_CIRKWI_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(CPSKIN_CIRKWI_FIXTURE,),
+    name='CpskinCirkwiLayer:IntegrationTesting'
+)
+
+
+CPSKIN_CIRKWI_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(CPSKIN_CIRKWI_FIXTURE,),
+    name='CpskinCirkwiLayer:FunctionalTesting'
+)
+
+
+CPSKIN_CIRKWI_ACCEPTANCE_TESTING = FunctionalTesting(
+    bases=(
+        CPSKIN_CIRKWI_FIXTURE,
+        REMOTE_LIBRARY_BUNDLE_FIXTURE,
+        z2.ZSERVER_FIXTURE
+    ),
+    name='CpskinCirkwiLayer:AcceptanceTesting'
+)
